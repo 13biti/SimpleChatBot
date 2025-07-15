a56dca08b3c3fc4f1153d1ef070366c941ee2fc4
k13 <mihome013@gmail.com>
2025-07-15 10:26:42 +0330

    clean up , add interaction with user

--- main.py
+++ main.py
@@ -1,63 +1,90 @@
-import nltk
 from nltk.stem.lancaster import LancasterStemmer
-
-stemmer = LancasterStemmer()
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Input
 from tensorflow.keras.optimizers import Adam
-import numpy as np
 import json
 import os
+import random
+
+stemmer = LancasterStemmer()
 
 
-# reading data from json file , tokenize it and create input and label for output
-def preprocess_data():
-    # this 3 are our vocab - token map - array of labels
-    words: list[str] = []
-    token_map: dict[str, int] = {}
-    labels: list[str] = []
-    # this is are our individual inputs ,
-    docs_query: list[list[str]] = []
-    docs_label: list[str] = []
-    with open("./intents.json") as f:
-        data = json.load(f)
-    for intent in data["intents"]:
-        for pattern in intent["patterns"]:
-            pattern = (
-                pattern.replace(",", "")
-                .replace("!", "")
-                .replace("?", "")
-                .replace("(", "")
-                .replace(")", "")
-                .replace(":", "")
-            )
-            stemmed_pattern = [stemmer.stem(w.lower()) for w in pattern.split(" ")]
-            for word in stemmed_pattern:
-                if word not in token_map.keys():
-                    token_map[word] = len(token_map) + 3
-                    words.append(word)
-            docs_query.append(stemmed_pattern)
-            docs_label.append(intent["tag"])
+class preprocessing:
+    def __init__(self) -> None:
+        # this 3 are our vocab - token map - array of labels
+        self.words: list[str] = []
+        self.token_map: dict[str, int] = {}
+        self.labels: list[str] = []
+        # this is are our individual inputs ,
+        self.docs_query: list[list[str]] = []
+        self.docs_label: list[str] = []
 
-    labels = sorted(list(set(docs_label)))
-    words = sorted(words)
-    token_map = {"<START>": 0, "<PAD>": 1, "<UNKNOWN>": 2, **token_map}
-    training_input = np.zeros((len(docs_query), len(words)))
-    training_output = np.zeros((len(docs_query), len(labels)))
-    for index, item in enumerate(docs_query):
-        for w in item:
-            # i know for fact that they are , but check if so if in future change previos code , this remain the same
-            if w in words:
-                training_input[index][words.index(w)] = 1
-        training_output[index][labels.index(docs_label[index])] = 1
-    # bag of word is working
-    return training_input, training_output
+    # reading data from json file , tokenize it and create input and label for output
+    #
+    def preprocess_data(self):
+        words: list[str] = []
+        token_map: dict[str, int] = {}
+        with open("./intents.json") as f:
+            data = json.load(f)
+        for intent in data["intents"]:
+            for pattern in intent["patterns"]:
+                pattern = (
+                    pattern.replace(",", "")
+                    .replace("!", "")
+                    .replace("?", "")
+                    .replace("(", "")
+                    .replace(")", "")
+                    .replace(":", "")
+                )
+                stemmed_pattern = [stemmer.stem(w.lower()) for w in pattern.split(" ")]
+                for word in stemmed_pattern:
+                    if word not in token_map.keys():
+                        token_map[word] = len(token_map) + 3
+                        words.append(word)
+                self.docs_query.append(stemmed_pattern)
+                self.docs_label.append(intent["tag"])
+
+        self.labels = sorted(list(set(self.docs_label)))
+        self.words = sorted(words)
+        self.token_map = {"<START>": 0, "<PAD>": 1, "<UNKNOWN>": 2, **token_map}
+        training_input = np.zeros((len(self.docs_query), len(self.words)))
+        training_output = np.zeros((len(self.docs_query), len(self.labels)))
+        for index, item in enumerate(self.docs_query):
+            for w in item:
+                # i know for fact that they are , but check if so if in future change previos code , this remain the same
+                if w in self.words:
+                    training_input[index][self.words.index(w)] = 1
+            training_output[index][self.labels.index(self.docs_label[index])] = 1
+        # bag of word is working
+        return training_input, training_output
+
+    def tokenize_user_input(self, input):
+        # there is not control over user input size
+        output_bow = np.zeros((1, len(self.words)))
+        input = (
+            input.replace(",", "")
+            .replace("!", "")
+            .replace("?", "")
+            .replace("(", "")
+            .replace(")", "")
+            .replace(":", "")
+        )
+        for w in input.split(" "):
+            if w in self.words:
+                output_bow[0][self.words.index(w)] = 1
+        return output_bow
+
+    def get_lable(self, index):
+        return self.labels[index]
+
+
+preprocessor = preprocessing()
+training_input, training_output = preprocessor.preprocess_data()
 
 
 def train_model():
-    training_input, training_output = preprocess_data()
     tf.keras.backend.clear_session()
     model = Sequential(
         [
@@ -70,7 +97,7 @@ def train_model():
     model.compile(
         optimizer=Adam(), loss="categorical_crossentropy", metrics=["accuracy"]
     )
-    model.fit(training_input, training_output, epochs=100, batch_size=8, verbose=1)
+    model.fit(training_input, training_output, epochs=300, batch_size=8, verbose=1)
     model.save("model.keras")
     return model
 
@@ -83,3 +110,24 @@ if os.path.exists("model.keras"):
     model = load_model()
 else:
     model = train_model()
+
+
+def interact_with_user():
+    print("hi , how are you doing to day ? ask me , or quit with(exit) :")
+    while True:
+        user_input = input("you:")
+        if user_input.lower() == "exit":
+            break
+        tokenize_user_input = preprocessor.tokenize_user_input(user_input)
+        predict_result = model.predict(tokenize_user_input)
+        predict_result = np.argmax(predict_result)
+        predict_result = preprocessor.get_lable(predict_result)
+        with open("./intents.json") as f:
+            data = json.load(f)
+        for item in data["intents"]:
+            if item["tag"] == predict_result:
+                answer = random.choice(item["responses"])
+                print("assistanse:", answer)
+
+
+interact_with_user()
